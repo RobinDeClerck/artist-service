7c75aa6a7844f8d256f51e665dc8cf5147afff71
Robin De Clerck <robin.de.clerck@gmail.com>
2021-12-10 13:50:35 +0100

    Update Artist.java

--- src/main/java/fact/it/artistservice/model/Artist.java
+++ src/main/java/fact/it/artistservice/model/Artist.java
@@ -9,6 +9,10 @@ public class Artist {
     private String id;
     private String name;
 
+    public Artist(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public String getId() {
         return id;
